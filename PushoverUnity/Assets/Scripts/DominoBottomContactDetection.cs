baa267058f0d511c1967472101212d02544f0708
Milan Assuied <milan.assuied@unity3d.com>
2023-03-02 19:45:45 -0500

    Remove physics

--- PushoverUnity/Assets/Scripts/DominoBottomContactDetection.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class DominoBottomContactDetection : MonoBehaviour
-{
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        Debug.Log(col.gameObject.name);
-    }
-}
